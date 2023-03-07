01e4f3642d5b95e0be2f8fe5343d09995ebbe280
Drew Wheeler <wheelercs@protonmail.com>
2023-03-07 17:24:18 -0600

    Threading and Subprocess Work
    Work has been done to make the Launcher class have the ability to spawn programs on their own threads. Currently it works, but there is not way yet to tell if a process has terminated, which is important.

--- src/Launcher.py
+++ src/Launcher.py
@@ -1,4 +1,46 @@
+from subprocess import Popen
+from sys import argv
+from threading import Thread
+from time import sleep
+
+
+class Launcher (object):
+    def __init__ (self) -> None:
+        self.path: str  = ""
+        self.args: list = []
+        self.proc: Popen = None
+        self.thread: Thread = None
+
+
+    def run_program (self) -> None:
+        self.thread = Thread (target = self.branch_proc)
+        self.thread.start()
+
+    def branch_proc (self) -> None:
+        self.proc = Popen([self.path] + self.args)
+
+
+    def proc_status (self) -> bool:
+        if (self.proc is not None):
+            self.proc.communicate()
+            return self.proc.returncode
+        else:
+            return False
+
+    def set_path (self, filepath: str) -> None:
+        self.path = filepath
+
+    def set_args (self, arg_list: str) -> None:
+        self.args = arg_list
+
 
 
 if __name__ == "__main__":
-    pass
+    my_prog = Launcher ()
+    my_prog.set_path(argv[1])
+    if len(argv) > 2:
+        my_prog.set_args(argv[2:])
+    my_prog.run_program()
+
+    while (True):
+        my_prog.proc_status()
